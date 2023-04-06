8a06ebe484714b7ff8e7b1414fda5f7be18a6ef3
TranHuyHiep <tranhuyhiep0710@gmail.com>
2023-04-06 20:26:25 +0700

    done

---
+++ Lap_trinh_web/Kiemtra/KiemTraLan2/ViewComponents/TranDauMenuViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using KiemTraLan2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KiemTraLan2.ViewComponents
+{
+    public class TranDauMenuViewComponent : ViewComponent
+    {
+        QlbongDaContext db = new QlbongDaContext();
+        public TranDauMenuViewComponent() { }
+
+        public IViewComponentResult Invoke()
+        {
+            var lstTranDau = db.Trandaus.ToList();
+            return View(lstTranDau);
+        }
+    }
+}
