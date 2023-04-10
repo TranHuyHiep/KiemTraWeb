39ab5a3718ae12f360f55a85f23c1881606ee9ce
TranHuyHiep <tranhuyhiep0710@gmail.com>
2023-04-10 10:53:04 +0700

    done index

---
+++ Lap_trinh_web/BaiTapLon/BTLWeb/BTLWeb/Controllers/ShoppingCartController.cs
@@ -0,0 +1,156 @@
+﻿using BTLWeb.Constants;
+using BTLWeb.Models;
+using BTLWeb.Models.ViewModels;
+using BTLWeb.Service;
+using Microsoft.AspNetCore.Mvc;
+using Nancy.Json;
+using System.Text.Json;
+
+namespace BTLWeb.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        QlbanMayAnhContext db = new QlbanMayAnhContext();
+        HoaDonBanService hoaDonBanService = new HoaDonBanService();
+
+        // GET: ShoppingCartController1
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult Add(string productId)
+        {
+            var cart = HttpContext.Session.Get<List<ShoppingCartViewModel>>(CommonConstants.SessionCart);
+            if (cart == null)
+            {
+                cart = new List<ShoppingCartViewModel>();
+            }
+            if (cart.Any(x => x.ProductId == productId))
+            {
+                foreach (var item in cart)
+                {
+                    if (item.ProductId == productId)
+                    {
+                        item.Quantity += 1;
+                    }
+                }
+            }
+            else
+            {
+                ShoppingCartViewModel newItem = new ShoppingCartViewModel();
+                newItem.ProductId = productId;
+                var product = db.TDanhMucSps.Find(productId);
+                newItem.Product = product;
+                newItem.Quantity = 1;
+                cart.Add(newItem);
+            }
+
+            HttpContext.Session.Set<List<ShoppingCartViewModel>>(CommonConstants.SessionCart, cart);
+
+            return Json(new
+            {
+                status = true
+            });
+        }
+
+        [HttpGet]
+        public JsonResult GetAll()
+        {
+            var cart = HttpContext.Session.Get<List<ShoppingCartViewModel>>(CommonConstants.SessionCart);
+            if (cart == null)
+            {
+                cart = new List<ShoppingCartViewModel>();
+            }
+            return Json(cart);
+        }
+
+        [HttpPost]
+        public JsonResult Update(string cartData)
+        {
+            var cartViewModel = new JavaScriptSerializer().Deserialize<List<ShoppingCartViewModel>>(cartData);
+
+            var cartSession = HttpContext.Session.Get<List<ShoppingCartViewModel>>(CommonConstants.SessionCart);
+            foreach (var item in cartSession)
+            {
+                foreach (var jitem in cartViewModel)
+                {
+                    item.Quantity = jitem.Quantity;
+                }
+            }
+
+            HttpContext.Session.Set<List<ShoppingCartViewModel>>(CommonConstants.SessionCart, cartSession);
+            return Json(new
+            {
+                status = true
+            });
+        }
+
+        [HttpPost]
+        public JsonResult DeleteAll()
+        {
+
+            HttpContext.Session.Set<List<ShoppingCartViewModel>>(CommonConstants.SessionCart, new List<ShoppingCartViewModel>());
+            return Json(new
+            {
+                status = true
+            });
+        }
+
+        public ActionResult Checkout()
+        {
+            var cart = HttpContext.Session.Get<List<ShoppingCartViewModel>>(CommonConstants.SessionCart);
+
+            if (cart == null)
+            {
+                return Redirect("/");
+            }
+            return View();
+        }
+
+        public JsonResult CreateOrder(string orderViewModel)
+        {
+            var order = new JavaScriptSerializer().Deserialize<OrderViewModel>(orderViewModel);
+            order.NgayHoaDon = DateTime.Now.ToString();
+            var orderNew = new THoaDonBan();
+
+            orderNew.UpdateOrder(order);
+
+            var cart = HttpContext.Session.Get<List<ShoppingCartViewModel>>(CommonConstants.SessionCart);
+            if(cart == null || cart.Count() == 0)
+            {
+                return Json(new
+                {
+                    status = false
+                });
+            }
+            List<TChiTietHdb> orderDetails = new List<TChiTietHdb>();
+             
+            foreach (var item in cart)
+            {
+                var detail = new TChiTietHdb();
+                detail.MaSp = item.ProductId;
+                detail.SoLuongBan = item.Quantity;
+                detail.DonGiaBan = item.Product.GiaLonNhat;
+                orderDetails.Add(detail);
+            }
+
+            if(hoaDonBanService.Create(orderNew, orderDetails) == true)
+            {
+                return Json(new
+                    {
+                        status = true
+                    });
+            }
+            else
+            {
+                return Json(new
+                {
+                    status = false
+                });
+            }
+            
+        }
+    }
+}
---
+++ Lap_trinh_web/BaiTapLon/BTLWeb/BTLWeb/wwwroot/Camera/js/ShoppingCart.js
@@ -0,0 +1,121 @@
+﻿
+
+
+function loadData() {
+    $.ajax({
+        url: 'https://localhost:44368/shoppingcart/getall',
+        type: 'GET',
+        dataType: 'json',
+        success: function (res) {
+            console.log(res)
+            var str = '';
+            var count = 0;
+            res.forEach(function (item) {
+                count += 1;
+                var money = item.product.giaLonNhat * item.quantity;
+                str +=
+                    `<tr>
+                            <td>${count}</td>
+                            <td><img src="../img/ImageCamera/${item.product.anhDaiDien}" alt="" style="width: 50px;"></td>
+                            <td>${item.product.tenSp}</td>
+                            <td class="align-middle">${item.product.giaLonNhat}</td>
+                            <td class="align-middle">
+                                <div class="input-group quantity mx-auto" style="width: 100px;">
+                                    <div class="input-group-btn">
+                                        <button class="btn btn-sm btn-primary btn-minus">
+                                            <i class="fa fa-minus"></i>
+                                        </button>
+                                    </div>
+                                    <input type="text" class="form-control form-control-sm bg-secondary border-0 text-center" value="1">
+                                    <div class="input-group-btn">
+                                        <button class="btn btn-sm btn-primary btn-plus">
+                                            <i class="fa fa-plus"></i>
+                                        </button>
+                                    </div>
+                                </div>
+                            </td>
+                            <td class="align-middle" id="amount_${count}">${money}</td>
+                            <td class="align-middle"><button class="btn btn-sm btn-danger"><i class="fa fa-times"></i></button></td>
+                        </tr>`
+            })
+
+            if (count == 0) {
+                str = "<tr><td><p>Giỏ hàng trống</p></td></tr>"
+            }
+
+            $('#showCart').html(str);
+        }
+    })
+}
+
+loadData();
+
+function btnDeleteAll() {
+    $.ajax({
+        url: 'https://localhost:44368/shoppingcart/DeleteAll',
+        type: 'POST',
+        dataType: 'json',
+        success: function (res) {
+            if (res.status) {
+                alert('Đã xóa giỏ hàng!');
+            } else {
+                alert('Bạn không có sản phẩm nào để xóa!')
+            }
+            loadData();
+        }
+    })
+}
+
+function btnContinue() {
+    window.location.href = "../";
+}
+
+function btnCheckout() {
+    var hoaDonBan = {
+        TongTienHD: 111,
+        PhuongThucThanhToan: 0,
+        GhiChu: 'cc',
+        TrangThai: 0,
+        MaKhachHang: 'N01'
+    }
+
+    $.ajax({
+        url: 'https://localhost:44368/shoppingcart/CreateOrder',
+        type: 'POST',
+        dataType: 'json',
+        data: {
+            orderViewModel: JSON.stringify(hoaDonBan)
+        },
+        success: function (res) {
+            if (res.status) {
+                alert("Mua hàng thành công!");
+                btnDeleteAll();
+            } else {
+                alert("Bạn chưa có sản phẩm nào trong giỏ hàng!")
+            }
+        }
+    })
+}
+function changeMoney() {
+
+}
+
+(function ($) {
+    "use strict";
+// Product Quantity
+$('.quantity button').on('click', function () {
+    alert("click")
+    var button = $(this);
+    var oldValue = button.parent().parent().find('input').val();
+    if (button.hasClass('btn-plus')) {
+        var newVal = parseFloat(oldValue) + 1;
+    } else {
+        if (oldValue > 0) {
+            var newVal = parseFloat(oldValue) - 1;
+        } else {
+            newVal = 0;
+        }
+    }
+    button.parent().parent().find('input').val(newVal);
+});
+})(jQuery);
